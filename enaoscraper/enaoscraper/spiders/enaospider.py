d86f7a4d0d96645c3bd04cb55689c64ace691147
Kalia Dorgelo <kaliadorgelo@gmail.com>
2025-07-03 11:30:10 +0200

    Adding things

--- enaoscraper/enaoscraper/spiders/enaospider.py
+++ enaoscraper/enaoscraper/spiders/enaospider.py
@@ -7,10 +7,11 @@ class EnaospiderSpider(scrapy.Spider):
     start_urls = ["https://everynoise.com/everynoise1d.html"]
 
     def parse(self, response):
+        print(response.css('iframe #__next > div > div > div.TrackList_backgroundColorContainer__vm8ks.TrackListWidget_trackListContainer__zpYQe > div > div > ol > li.TracklistRow_trackListRow__vrAAd.TracklistRow_isCurrentTrack__N2KN6.TracklistRow_isPlayable__U6o2r > h3'))
         yield {
                 # #__next > div > div > div.TrackList_backgroundColorContainer__vm8ks.TrackListWidget_trackListContainer__zpYQe > div > div > ol > li.TracklistRow_trackListRow__vrAAd.TracklistRow_isCurrentTrack__N2KN6.TracklistRow_isPlayable__U6o2r > h3
-                'name':  response.css('li.TracklistRow_trackListRow__vrAAd.TracklistRow_isPlayable__U6o2r h3:attr(text)').get(),
-                'artist': response.css('li.TracklistRow_trackListRow__vrAAd.TracklistRow_isPlayable__U5o2r h4::attr(text)').get(),
-                'genre': response.css('tr:nth-child(1) td:nth-child(3) a:nth-child(3)').get(),
+                'name':  response.css('#__next > div > div > div.TrackList_backgroundColorContainer__vm8ks.TrackListWidget_trackListContainer__zpYQe > div > div > ol > li.TracklistRow_trackListRow__vrAAd.TracklistRow_isCurrentTrack__N2KN6.TracklistRow_isPlayable__U6o2r > h3').get(),
+                'artist': response.css('ol li.TracklistRow_trackListRow__vrAAd.TracklistRow_isPlayable__U5o2r h4::text').get(),
+                'genre': response.css('tr td.note a::text').get(),
         } 
 
