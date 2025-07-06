3c42ab63cf91272476c37b9ad52ad6b89c14c597
My-Hanh <dangmyh1@students.zhaw.ch>
2025-07-06 19:23:51 +0200

    Get one href with Selenium

--- web_scraping_result/everynoise_scraper/everynoise_scraper/spiders/everynoise_spider.py
+++ web_scraping_result/everynoise_scraper/everynoise_scraper/spiders/everynoise_spider.py
@@ -26,28 +26,29 @@ class EverynoiseSpiderSpider(scrapy.Spider):
 
     def parse(self, response):   
         logger.info("*********************** Starting parse function ***********************")
+    
         # Launch headless Chrome
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         logger.info("*********************** Launching headless Chrome ***********************")
+        
         driver = webdriver.Chrome(service=ChromeService(), options=options)
-
         driver.get(response.url)
         logger.info(f"*********************** Navigated to {response.url} ***********************")
         logger.info("*********************** Page loaded, waiting for genre links to appear ***********************")
         
-        relative_links = driver.find_elements(By.CSS_SELECTOR, "td.note > a[href*='everynoise1d-']")
-
+        relative_links = driver.find_elements(by=By.XPATH, value="/html/body/table/tbody/tr[1]/td[3]/a")
         logger.info(f"*********************** Found {len(relative_links)} relative links ***********************")
-
+        
+        # Extract href attributes
         genre_links = [a.get_attribute("href") for a in relative_links]
         
-        logger.info("*********************** Page loaded, scraping genre links ***********************")
-
+        
+        
+        logger.info("*********************** Scraping complete ***********************")
         driver.quit()
-
         logger.info(f"*********************** Found {len(genre_links)} genre links ***********************")
         
         # for href in genre_links[:1]:               
