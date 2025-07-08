63c38c7ce47f773b1279556c3cf63d4d24f87123
Anna Baranek <baranann@students.zhaw.ch>
2025-07-08 11:40:11 +0200

    made the code more understandable

--- enaoscraper/enaoscraper/ascii_cleaning.py
+++ enaoscraper/enaoscraper/ascii_cleaning.py
@@ -2,10 +2,10 @@ import json
 import os
 
 #Path to the json with the links and genre of the songs
-folder = "C:\\Users\\annab\\Ampli-FIRE\\web_scraping_result\\everynoise_scraper\\everynoise_scraper\\"
+folder = "YOUR PATH WHERE YOU WANT TO SAVE YOUR FILE IN AT THE END"
 
 #name of the json file, that the scraped data would be saved to
-filename = os.path.join(folder, "music_data_readable.json")
+filename = os.path.join(folder, "NAME OF THE FILE")
 
 # Beispiel: So würde man eine JSON-Datei laden
 with open("web_scraping_result\everynoise_scraper\everynoise_scraper\music_data.json", "r", encoding="utf-8") as f:
@@ -18,5 +18,5 @@ for song in music_data:
 
 
 # Save properly encoded JSON with readable characters
-with open(filename, "w", encoding="utf-8") as file:
+with open(filename, "w", encoding="utf-8") as file: #filename or "FILE NAME YOU WANT IT TO BE CALLED"
     json.dump(music_data, file, indent=4, ensure_ascii=False)
