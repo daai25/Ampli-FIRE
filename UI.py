72b3e04a99e6e2a610e1240f3a1c3b6950b040bd
DorgeloK <155837067+DorgeloK@users.noreply.github.com>
2025-07-22 10:40:47 -0400

    Trying to align the logo again

--- UI.py
+++ UI.py
@@ -1,14 +1,8 @@
 import streamlit as st
 
-#st.image("Amplifire_logo.png", width=150)
-st.markdown(
-    """
-    <div style="text-align: center;">
-        <img src="Amplifire_logo.png" width="100">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("Amplifire_logo.png", width=150)
 
 # Configuring Page
 st.set_page_config(page_title="Upload or Enter Text", layout="centered")
